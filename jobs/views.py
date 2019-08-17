e7b9419aa475f88ae899973c041f264ee3b750b7
kwonyongju <39615232+kwonyongju@users.noreply.github.com>
2019-08-16 22:53:44 -0700

    Update views.py

--- jobs/views.py
+++ jobs/views.py
@@ -4,3 +4,7 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detail_blog})
